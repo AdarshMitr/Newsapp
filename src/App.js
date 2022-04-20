ea6c6ec51f818f68be13f4fa4ea6c2ac0477ac34
AdarshMitr <adarshkumar019@gmail.com>
2022-04-20 13:17:51 +0530

    Adding a top loading bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+
 
 import {
   BrowserRouter,
@@ -11,31 +13,43 @@ import {
   Route,
 } from "react-router-dom";
 export default class App extends Component {
-  pageSize=15;
+  pageSize = 15;
+  state={
+    progress:0
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <div>
         <BrowserRouter>
- <Navbar/>
-       
-        <Routes>
-       
-       <Route>
-        <Route exact path="business" element = {<News key="business" pageSize = {this.pageSize} country = "in" category = "Business"/>}/>   
-        <Route exact path="entertainment" element = { <News key= "entertainment"pageSize = {this.pageSize} country = "in" category = "Entertainment"/>}/> 
-        <Route exact path="general" element = {<News key="general" pageSize = {this.pageSize} country = "in" category = "General"/>}/> 
-        <Route exact path="health" element = {<News  key="health" pageSize = {this.pageSize} country = "in" category = "Health"/>}/> 
-        <Route exact path="science" element = {<News  key="science" pageSize = {this.pageSize} country = "in" category = "Science"/>}/> 
-        <Route exact path="sports" element = {<News  key="sports" pageSize = {this.pageSize} country = "in" category = "Sports"/>}/> 
-        <Route exact path="technology" element = {<News key= "technology" pageSize = {this.pageSize} country = "in" category = "Technology"/>}/>       
-               
+          <Navbar />
+          <LoadingBar
+          height={3}
+        color='#f11946'
+        progress={this.state.progress}
+      
+      />
 
-        </Route>
-        </Routes>
-        
+          <Routes>
+
+            <Route>
+              <Route exact path="business" element={<News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="in" category="Business" />} />
+              <Route exact path="entertainment" element={<News setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} country="in" category="Entertainment" />} />
+              <Route exact path="general" element={<News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="General" />} />
+              <Route exact path="health" element={<News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country="in" category="Health" />} />
+              <Route exact path="science" element={<News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country="in" category="Science" />} />
+              <Route exact path="sports" element={<News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country="in" category="Sports" />} />
+              <Route exact path="technology" element={<News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country="in" category="Technology" />} />
+
+
+            </Route>
+          </Routes>
+
         </BrowserRouter>
-        </div>
-        
+      </div>
+
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,17 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+          <div style={
+            {
+              display: 'flex',
+              justifyContent: 'flex-end',
+              position: 'absolute',
+              right: '0',
+            }
+          }>
+            <span className="badge rounded-pill bg-danger" >{source}</span>
+          </div>
+
           <img src={!imageUrl ? "https://c.ndtvimg.com/2019-08/b1q4llg8_maryam-nawaz650_625x300_08_August_19.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}
